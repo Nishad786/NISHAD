5f5de209fea79ce37a5724bf21512d2a6dc8f119
Nishad786 <engaming1211@gmail.com>
2020-11-11 16:52:33 +0530

    commit by nishad

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundimg;
+var platform;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function preload(){
+   backgroundimg=loadImage("sprites/bg.png");
 }
 
 function setup(){
@@ -15,9 +16,10 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform=new Ground(1500,305,300,170);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -39,7 +41,7 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    background(backgroundimg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -60,5 +62,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
     platform.display();
 }
